a9b9a75d439cb7370659c37a0f9661d48fa910fb
Stephen Cunningham <stevecunningham@outlook.com>
2023-05-11 21:56:52 -0500

    create endpoint and get by id

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"net/http" //built in
 
+	"errors"
+
 	"github.com/gin-gonic/gin" //downloaded go-get
-	//"errors"
 )
 
 //set up dependency tracking
@@ -32,8 +33,44 @@ func getBooks(c *gin.Context) {
 
 	c.IndentedJSON(http.StatusOK, books)
 }
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+
+	return nil, errors.New("book not found")
+}
+
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"}) //H allows customized json
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func addBook(c *gin.Context) { // context stores all info related to specific req
+	var newBook book
+	// use c to bind json data payload to this new book object
+	if err := c.BindJSON(&newBook); err != nil {
+		return //the BindJson returns the error response auto if err is not null
+	}
+
+	books = append(books, newBook)              //add book to list
+	c.IndentedJSON(http.StatusCreated, newBook) //return the book we just created
+
+}
 func main() {
 	router := gin.Default() //create router to handle routes
 	router.GET("/books", getBooks)
+	router.POST("/books", addBook)
+	router.GET("/books/:id", bookById)
 	router.Run("localhost:8080")
 }
